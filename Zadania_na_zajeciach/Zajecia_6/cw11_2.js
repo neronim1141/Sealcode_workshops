2a11886742eb43e8c84f0aea9ca5e33b28d75d0a
Kacper Kruczek <neronim66@gmail.com>
2017-12-11 16:09:02 +0100

    cw11_2

---
+++ Zadania_na_zajeciach/Zajecia_6/cw11_2.js
@@ -0,0 +1,10 @@
+
+let liArray = document.getElementsByTagName("li");
+for(let i=0; i<liArray.length;i++){
+    console.log(liArray[i].innerHTML);
+}
+
+let exampleArray = document.getElementsByClassName("example");
+for(let i=0; i<exampleArray.length;i++){
+    console.log(exampleArray[i].innerHTML);
+}
